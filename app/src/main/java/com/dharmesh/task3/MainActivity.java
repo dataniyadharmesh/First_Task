a54e0b5bb6c0122b016f8dae5807cf5cc75172f6
dataniyadharmesh <dataniyadharmesh3132@gmail.com>
2017-02-02 10:20:20 +0530

    Comment Deleted

--- app/src/main/java/com/dharmesh/task3/MainActivity.java
+++ app/src/main/java/com/dharmesh/task3/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-// First Comment
+
 public class MainActivity extends AppCompatActivity {
     private Fragment f1;
 
